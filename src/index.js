df9190f4a5cf890035d33f998dbc58de02cdf983
Artem Repetskyi <luckiarti@Artems-MacBook-Pro.local>
2020-10-07 16:59:05 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import js from './js/index';
 import scss from './scss/main.scss';
-// import('./js/plugins/index.js').then(Red => {
-// 	var test = Red.default;
-// 	test.alert('Тут можно текст 4');
-// });
-
